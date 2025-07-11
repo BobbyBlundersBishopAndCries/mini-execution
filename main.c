c7e53697a69faa0a31ff939e5fea875bca6bd7c6
Mohamed Lakhdar <mlakhdar@c4r5p10.1337.ma>
2025-07-11 20:44:10 +0100

    ownedbyhunter

--- main.c
+++ main.c
@@ -3,61 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: med <med@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: mlakhdar <mlakhdar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 18:58:35 by feedback          #+#    #+#             */
-/*   Updated: 2025/07/10 12:00:23 by med              ###   ########.fr       */
+/*   Updated: 2025/07/11 20:43:13 by mlakhdar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_shell_state	g_shell = {.exit_status = 0, .sigint_received = 0,
-		.in_heredoc = 0, .child_running = 0};
-		
-int	main(int argc, char **argv, char **envp)
-{
-	char		*input;
-	t_lst_cmd	*cmds;
-	t_cmd		*cmd;
-	t_env		*envir;
+t_shell_state g_shell = {.exit_status = 0, .sigint_received = 0, .in_heredoc = 0, .child_running = 0};
 
-	(void)argc;
-	(void)argv;
-	envir = get_env(envp);
+// Assume these types and functions are defined elsewhere:
+// t_env, t_lst_cmd, t_cmd, g_shell, get_env, handle_signals, parsing,
+// env_tochar, is_builtin, execute_builtin, execute_pipeline, free_all
+
+static void handle_input_signals(void)
+{
+	handle_signals();
+	g_shell.child_running = 0;
+}
+static char *get_user_input(void)
+{
+	char *input = readline("minishell:$ ");
+	if (!input)
+		write(1, "exit\n", 5);
+	return input;
+}
+
+static void setup_cmd_env(t_lst_cmd *cmds, t_env *envir)
+{
+	t_cmd *cmd = cmds->head;
+	while (cmd)
+	{
+		cmd->env = &envir;
+		cmd->envp = env_tochar(envir);
+		cmd = cmd->next;
+	}
+}
+
+static void execute_cmd_list(t_lst_cmd *cmds)
+{
+    t_cmd *cmd = cmds->head;
+    if (!cmd->next && is_builtin(cmd->args[0]))
+        g_shell.exit_status = execute_builtin(cmd);
+   	else
+		{
+        g_shell.child_running = 1;
+        execute_pipeline(cmd);
+        g_shell.child_running = 0;
+    }
+}
+
+static void minishell_loop(t_env *envir)
+{
+	char *input;
+	t_lst_cmd *cmds;
+
 	while (1)
 	{
-		handle_signals();
-		g_shell.child_running = 0;
-		input = readline("\001\033[1;34m\002minishell:$ \001\033[0m\002");
+		handle_input_signals();
+		input = get_user_input();
 		if (!input)
-		{
-			write(1, "exit\n", 5);
-			break ;
-		}
+			break;
 		if (*input)
 			add_history(input);
 		cmds = parsing(input, envir, &g_shell.exit_status);
 		free(input);
 		if (!cmds || !cmds->head)
-			continue ;
-		cmd = cmds->head;
-		while (cmd)
-		{
-			cmd->env = &envir;
-			cmd->envp = env_tochar(envir);
-			cmd = cmd->next;
-		}
-		cmd = cmds->head;
-		if (!cmd->next && is_builtin(cmd->args[0]))
-			g_shell.exit_status = execute_builtin(cmd);
-		else
-		{
-			g_shell.child_running = 1;
-			execute_pipeline(cmd);
-			g_shell.child_running = 0;
-		}
+			continue;
+		setup_cmd_env(cmds, envir);
+		execute_cmd_list(cmds);
 		free_all(cmds->k);
 	}
-	return (g_shell.exit_status);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    t_env *envir;
+
+    (void)argc;
+    (void)argv;
+    envir = get_env(envp);
+    minishell_loop(envir);
+    return g_shell.exit_status;
 }
